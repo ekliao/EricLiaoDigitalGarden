3ee996e2fb8fcfd6826c47485261e095a7b54c77
Eric Liao <ekliao@gmail.com>
2023-04-16 12:08:57 +0800

    Initialize filetree

---
+++ src/site/_data/meta.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const axios = require("axios");
+
+
+module.exports = async() => {
+    let themeStyle = "";
+    if (process.env.THEME) {
+        const res = await axios.get(process.env.THEME)
+        themeStyle = `<style>${res.data}</style>`;
+    }
+    const meta ={
+        env: process.env.ELEVENTY_ENV,
+        theme: process.env.THEME,
+        themeStyle: themeStyle,
+        baseTheme: process.env.BASE_THEME || "dark",
+        siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
+    };
+
+    return meta;
+};
